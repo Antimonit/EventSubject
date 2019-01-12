6c9a5635a2904e93d3d74d550c119a4f3eb96f7f
David Khol <david@khol.me>
2019-01-12 19:17:47 +0100

    Updated tests.

--- eventsubject/src/test/java/me/khol/reactive/subjects/SubjectsTests.java
+++ eventsubject/src/test/java/me/khol/reactive/subjects/SubjectsTests.java
@@ -1,44 +1,62 @@
 package me.khol.reactive.subjects;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import io.reactivex.observers.TestObserver;
 
 public class SubjectsTests {
 
+	private EventSubject<Integer> subject;
+
+	@Before
+	public void setUp() {
+		subject = EventSubject.create();
+	}
+
 	@Test
-	public void testEventSubject() {
-		EventSubject<Integer> subject = EventSubject.create();
-
+	public void testOnlyOneSubscriber() {
 		TestObserver<Integer> to1 = subject.test();
-
-		// fresh EventSubjects are empty
-		to1.assertEmpty();
-
 		TestObserver<Integer> to2 = subject.test();
-
-		// A EventSubject only allows one Observer subscribed at a time
 		to2.assertFailure(IllegalStateException.class);
+	}
+
+	@Test
+	public void testDefaultEmpty() {
+		TestObserver<Integer> to1 = subject.test();
+		to1.assertEmpty();
+	}
+
+	@Test
+	public void testImmediateValues() {
+		TestObserver<Integer> to1 = subject.test();
 
 		subject.onNext(1);
 		to1.assertValue(1);
 
 		subject.onNext(2);
 		to1.assertValues(1, 2);
+	}
+
+	@Test
+	public void testQueuedValues() {
+		subject.onNext(1);
+		subject.onNext(2);
+
+		TestObserver<Integer> to1 = subject.test();
+		to1.assertValues(1, 2);
+	}
+
+	@Test
+	public void testResubscribe() {
+		TestObserver<Integer> to1 = subject.test();
+		subject.onNext(1);
+		to1.assertValues(1);
 
 		to1.dispose();
 
-		// a EventSubject caches events until an Observer subscribes
-		subject.onNext(3);
-		subject.onNext(4);
-
-		TestObserver<Integer> to3 = subject.test();
-
-		// the cached events are emitted in order
-		to3.assertValues(3, 4);
-
-		subject.onComplete();
-		to3.assertResult(3, 4);
+		TestObserver<Integer> to2 = subject.test();
+		subject.onNext(2);
+		to2.assertValues(2);
 	}
-
 }
